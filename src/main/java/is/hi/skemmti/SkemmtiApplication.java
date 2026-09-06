f413bd5fa07ea5fb67b07987cd422ed8243e6ed7
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-09-06 19:24:48 +0000

    java spring init

---
+++ src/main/java/is/hi/skemmti/SkemmtiApplication.java
@@ -0,0 +1,13 @@
+package is.hi.skemmti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkemmtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkemmtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/hi/skemmti/SkemmtiApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.skemmti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkemmtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
