9696eee3250a22677d44b5ded7cc594f8792ba70
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-09-21 11:43:23 +0000

    feat: db seeding + lombok for getters, setters, constructors & more

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (user_id, username, image, description, role) VALUES
+    (1, 'admin', NULL, 'admin', 'ADMIN'),
+    (2, 'Októberfest', NULL, 'Októberfest Háskóla Íslands', 'ORGANIZER'),
+    (3, 'Jón Jónsson', NULL, 'Venjulegur náungi', 'MEMBER');
+
+INSERT INTO event (event_id, name, date, start_time, location, description, tag, image, organizer_user_id, published) VALUES
+    (1, 'Októberfest dagur 1', '2026-09-10', '17:00:00', 'Háskóli Íslands', 'Októberfest!', 'Októberfest', NULL, 2, true),
+    (2, 'Októberfest dagur 2', '2026-09-11', '16:00:00', 'Háskóli Íslands', 'Októberfest!', '', NULL, 2, true),
+    (3, 'Októberfest dagur 3', '2026-09-12', '16:00:00', 'Háskóli Íslands', 'Októberfest!', '', NULL, 2, false);
+
+INSERT INTO users_attending_events (user_user_id, attending_events_event_id) VALUES
+    (3, 1),
+    (3, 2);
+
+INSERT INTO users_favorite_organizers (user_user_id, favorite_organizers_user_id) VALUES
+    (3, 2);
+
+ALTER TABLE users ALTER COLUMN user_id RESTART WITH 100;
+ALTER TABLE event ALTER COLUMN event_id RESTART WITH 100;
